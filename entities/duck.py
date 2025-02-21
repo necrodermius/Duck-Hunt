21e2634ed59e5210ae41ec1cc0958a4d49f9f45a
Elizabeth <lizokgrugorenchuk@gmail.com>
2025-02-21 11:01:36 +0200

    final sinusoidal motion of the duck

--- entities/duck.py
+++ entities/duck.py
@@ -7,18 +7,35 @@ class Duck:
         self.x = x
         self.start_y = y  
         self.y = y 
-        self.speed = 2
+        self.speed = 5 
+        self.amplitude = 30 
+        self.frequency = 0.05  
+        self.angle = 0  
 
-        self.image_down = pygame.image.load("assets/targets/4.png")
+        self.image_up = pygame.image.load("assets/targets/4.png")
+        self.image_down = pygame.image.load("assets/targets/3.png")
+
+        self.image_up = pygame.transform.scale(self.image_up, (50, 50))
         self.image_down = pygame.transform.scale(self.image_down, (50, 50))
 
-        self.image = self.image_down
+        self.image = self.image_up
         self.width, self.height = self.image.get_size()
+
         
     def update(self):
         self.x += self.speed
+        
         if self.x > 900:
             self.x = -50
+
+        previous_y = self.y
+        self.y = self.start_y + self.amplitude * math.sin(self.angle)
+        self.angle += self.frequency
+
+        if self.y > previous_y:
+            self.image = self.image_down
+        else:
+            self.image = self.image_up
 
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
