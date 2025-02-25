94f39153efda37a1581e928dbed0de2bf8099b3d
Maksym Putin <palach782@gmail.com>
2025-02-25 13:19:02 +0200

    Added some text on free_gamemode_scene

--- scenes/free_gamemode_scene.py
+++ scenes/free_gamemode_scene.py
@@ -9,9 +9,12 @@ class GameScene:
 
         self.game_bg = pygame.image.load("assets/bgs/free-play-bg.png").convert_alpha()
         self.game_bn = pygame.image.load("assets/banners/free-play-banner.png").convert_alpha()
+
         # Список качок
         self.gun = Gun()
-        # Лічильники
+        self.hits_count = 0
+        self.shots_count = 0
+        self.start_time = pygame.time.get_ticks()
 
     def handle_events(self, events):
         for event in events:
@@ -21,6 +24,8 @@ class GameScene:
                     self.scene_manager.set_scene("menu")
                 elif self.pause_rect.collidepoint(mouse_pos):
                     self.scene_manager.set_scene("pause")
+                else:
+                    self.shots_count += 1
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self.scene_manager.set_scene("pause")
 
@@ -33,9 +38,17 @@ class GameScene:
 
     def draw(self, screen):
         screen.blit(self.game_bg, (0, 0))
+        self.gun.draw(screen)
         screen.blit(self.game_bn, (0, 600))
         # Малюємо качок
-        self.gun.draw(screen)
-        # Малюємо кулі
-        # Малюємо текст (бали, кількість вистрілів)
+
+        current_time_ms = pygame.time.get_ticks() - self.start_time
+        current_time_sec = current_time_ms / 1000.0
+
+        font = pygame.font.SysFont("Times New Roman", 36)
+        time_surface = font.render(f"Час: {current_time_sec:.1f}", True, (255, 255, 255))
+        total_shots_surface = font.render(f"Постірілів відбулося {self.shots_count}", True, (255, 255, 255))
+        screen.blit(time_surface, (290, 615))
+        screen.blit(total_shots_surface, (290, 645))
+        #Збито качок
         pass
