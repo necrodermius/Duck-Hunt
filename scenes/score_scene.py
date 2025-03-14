1b9d1503cadc5505237334040a59202e431e1c0f
Maksym Putin <palach782@gmail.com>
2025-03-14 15:05:54 +0200

    Added some fixtures, mocked scene_manager, parametrized duck and sorted tests to the classes

--- scenes/score_scene.py
+++ scenes/score_scene.py
@@ -21,7 +21,7 @@ class ScoreScene:
     def handle_events(self, events):
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
+                mouse_pos = event.pos
                 if self.main_menu_rect.collidepoint(mouse_pos):
                     self.scene_manager.scenes["game"].restart()
                     self.scene_manager.scenes["ammo"].restart()
